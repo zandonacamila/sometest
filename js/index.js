448b3aadbfb2fcded12017f8df0621e2c4fc4ea1
zandonacamila <zandona.camila@gmail.com>
2020-07-31 11:30:44 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,40 @@
+const leftButton = $("#cantoEsquerdo")
+const middleButton = $("#meio");
+const rightButton = $("#cantoDireito");
+const addToList = $("#addInput");
+
+leftButton.on("click", addClassNameLeft);
+
+middleButton.on("click", addClassNameMiddle);
+
+rightButton.on("click", addClassNameRight);
+
+addToList.on("click", addItemToList);
+
+
+function addClassNameLeft() {
+
+    $("#left").toggleClass("hidden");
+}
+
+function addClassNameMiddle() {
+
+    $("#middle").toggleClass("hidden");
+}
+
+function addClassNameRight() {
+    
+    $("#right").toggleClass("hidden");
+}
+
+function addItemToList(event) {
+
+    event.preventDefault;
+    
+    const li = document.createElement("li")
+    const inputValue = $("#listInput").val();
+
+    li.textContent = inputValue;
+    $("#ulContent").append(li);
+
+}
